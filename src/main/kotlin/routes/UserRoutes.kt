a0efb373027a8f3bba6bbb16cbf019cd17d0cda7
Manuel Kour <manuelkour@gmail.com>
2021-07-29 12:38:21 +0200

    Bot Authorization

--- src/main/kotlin/routes/UserRoutes.kt
+++ src/main/kotlin/routes/UserRoutes.kt
@@ -7,10 +7,16 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import src.main.kotlin.models.ProfilePicture
+import java.nio.file.Files
+import java.nio.file.Path
 
+private val authorized_token: List<String> = Files.readAllLines(Path.of("src/main/resources/authorized_keys.txt"))
 
 fun Route.createUser() {
-    post("/user/{discordID}/{discordName}") {
+    post("/user/{token}/{discordID}/{discordName}") {
+        if (!authorized_token.contains(call.parameters["token"])) {
+            call.respondText("Unauthorized access.", status = HttpStatusCode.Unauthorized)
+        }
         if (userStorage.find { it.id == call.parameters["discordID"] } == null) {
             val user = call.parameters["discordName"]?.let { it1 -> User(call.parameters["discordID"], it1) }
             if (user != null) {
@@ -24,57 +30,79 @@ fun Route.createUser() {
 }
 
 fun Route.getUser() {
-    get("/user/{discordID}") {
-        val user = userStorage.find { it.id == call.parameters["discordID"] }
-        if (user == null) call.respondText("User doesn't exist", status = HttpStatusCode.NotFound)
-        else call.respond(user)
+    get("/user/{token}/{discordID}") {
+        if (!authorized_token.contains(call.parameters["token"])) {
+            call.respondText("Unauthorized access.", status = HttpStatusCode.Unauthorized)
+        } else {
+            val user = userStorage.find { it.id == call.parameters["discordID"] }
+            if (user == null) call.respondText("User doesn't exist", status = HttpStatusCode.NotFound)
+            else call.respond(user)
+        }
     }
 }
 
 
 fun Route.userSearch() {
-    get("/user/search/{wildcard}") { // WildCard = Discord ID or Name
-        val user: User?
-        val searchParameter: String? = call.parameters["wildcard"]
-        if (searchParameter != null && searchParameter.toLongOrNull() == null) {
-            user = userStorage.find { it.userName == searchParameter }
-        } else { user = userStorage.find { it.id == searchParameter } }
+    get("/user/{token}/search/{wildcard}") { // WildCard = Discord ID or Name
+        if (!authorized_token.contains(call.parameters["token"])) {
+            call.respondText("Unauthorized access.", status = HttpStatusCode.Unauthorized)
+        } else {
+            val user: User?
+            val searchParameter: String? = call.parameters["wildcard"]
+            if (searchParameter != null && searchParameter.toLongOrNull() == null) {
+                user = userStorage.find { it.userName == searchParameter }
+            } else {
+                user = userStorage.find { it.id == searchParameter }
+            }
 
-        if (user == null) call.respondText("User does not exist.", status = HttpStatusCode.NotFound)
-        else call.respond(user)
+            if (user == null) call.respondText("User does not exist.", status = HttpStatusCode.NotFound)
+            else call.respond(user)
+        }
     }
 }
 
 fun Route.postAnime() {
-    post("/user/{discordID}/anime/{name}") {
-        val user = userStorage.find { it.id == call.parameters["discordID"] }
-        if (user != null) {
-            user.addAnime(call.parameters["name"])
-            call.respondText("Anime added successfully.", status = HttpStatusCode.OK)
-        }  else {
-            call.respondText("User does not exist.", status = HttpStatusCode.NotFound)
+    post("/user/{token}/{discordID}/anime/{name}") {
+        if (!authorized_token.contains(call.parameters["token"])) {
+            call.respondText("Unauthorized access.", status = HttpStatusCode.Unauthorized)
+        } else {
+            val user = userStorage.find { it.id == call.parameters["discordID"] }
+            if (user != null) {
+                user.addAnime(call.parameters["name"])
+                call.respondText("Anime added successfully.", status = HttpStatusCode.OK)
+            } else {
+                call.respondText("User does not exist.", status = HttpStatusCode.NotFound)
+            }
         }
     }
 }
 
 fun Route.postProfilePicture() {
-    post("/user/{discordID}/profile") {
-        val picture = call.receive<ProfilePicture>().link
-        val user = userStorage.find { it.id == call.parameters["discordID"] }
-        if (user != null) {
-            user.setProfilePicture(picture)
-            call.respondText("Profile picture was set successfully.", status = HttpStatusCode.OK)
-        } else call.respondText("User does not exist.", status = HttpStatusCode.NotFound)
+    post("/user/{token}/{discordID}/profile") {
+        if (!authorized_token.contains(call.parameters["token"])) {
+            call.respondText("Unauthorized access.", status = HttpStatusCode.Unauthorized)
+        } else {
+            val picture = call.receive<ProfilePicture>().link
+            val user = userStorage.find { it.id == call.parameters["discordID"] }
+            if (user != null) {
+                user.setProfilePicture(picture)
+                call.respondText("Profile picture was set successfully.", status = HttpStatusCode.OK)
+            } else call.respondText("User does not exist.", status = HttpStatusCode.NotFound)
+        }
     }
 
 }
 
 fun Route.deleteUser() {
-    delete("/user/{discordID}") {
-        if(userStorage.removeIf{ it.id == call.parameters["discordID"]})
-            call.respondText("Deletion was successful.", status = HttpStatusCode.OK)
-        else
-            call.respondText("User does not exist.", status = HttpStatusCode.NotFound)
+    delete("/user/{token}/{discordID}") {
+        if (!authorized_token.contains(call.parameters["token"])) {
+            call.respondText("Unauthorized access.", status = HttpStatusCode.Unauthorized)
+        } else {
+            if (userStorage.removeIf { it.id == call.parameters["discordID"] })
+                call.respondText("Deletion was successful.", status = HttpStatusCode.OK)
+            else
+                call.respondText("User does not exist.", status = HttpStatusCode.NotFound)
+        }
     }
 }
 
--- src/test/kotlin/UserTests.kt
+++ src/test/kotlin/UserTests.kt
@@ -6,6 +6,8 @@ import models.Anime
 import models.userStorage
 import org.junit.Test
 import src.main.kotlin.models.ProfilePicture
+import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
@@ -14,13 +16,14 @@ import kotlin.test.assertTrue
 
 class UserTests {
 
+    private val token: String = Files.readAllLines(Path.of("src/main/resources/authorized_keys.txt"))[0]
 
     @Test
     fun getByNameAndID() {
         withTestApplication({ module(testing = true) }) {
 
             // Get own profile
-            handleRequest(HttpMethod.Get, "/user/166883258200621056").apply {
+            handleRequest(HttpMethod.Get, "/user/$token/166883258200621056").apply {
                 assertEquals(
                     """{"id":"166883258200621056","userName":"Asuha","profilePicture":"zero","animeList":[]}""",
                     response.content
@@ -28,28 +31,28 @@ class UserTests {
                 assertEquals(HttpStatusCode.OK, response.status())
             }
 
+            // With non authorized token
+            handleRequest(HttpMethod.Get, "/user/123456/166883258200621056").apply {
+                assertEquals("Unauthorized access.", response.content)
+                assertEquals(HttpStatusCode.Unauthorized, response.status())
+            }
+
 
             // Get by name
-            handleRequest(HttpMethod.Get, "/user/search/Iplayfair").apply {
-                val content = response.content
-                println("2. $content")
+            handleRequest(HttpMethod.Get, "/user/$token/search/Iplayfair").apply {
                 assertEquals(
                     """{"id":"137453944237457408","userName":"Iplayfair","profilePicture":"zero","animeList":[]}""",
-                    content
+                    response.content
                 )
-                // assertEquals(HttpStatusCode.OK, response.status())
             }
 
 
             // Get by ID
-            handleRequest(HttpMethod.Get, "/user/search/137453944237457408").apply {
-                val content = response.content
-                println("3. $content")
+            handleRequest(HttpMethod.Get, "/user/$token/search/137453944237457408").apply {
                 assertEquals(
                     """{"id":"137453944237457408","userName":"Iplayfair","profilePicture":"zero","animeList":[]}""",
-                    content
+                    response.content
                 )
-                // assertEquals(HttpStatusCode.OK, response.status())
             }
         }
     }
@@ -58,7 +61,7 @@ class UserTests {
     @Test
     fun postUser() {
         withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Post, "/user/137874349846822912/Weska").apply {
+            handleRequest(HttpMethod.Post, "/user/$token/137874349846822912/Weska").apply {
                 assertNotNull(userStorage.find { it.userName == "Weska" })
                 assertNotNull(userStorage.find { it.id == "137874349846822912" })
             }
@@ -69,7 +72,7 @@ class UserTests {
     @Test
     fun postAnime() {
         withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Post, "/user/166883258200621056/anime/JoJo").apply {
+            handleRequest(HttpMethod.Post, "/user/$token/166883258200621056/anime/JoJo").apply {
                 userStorage.find { it.id == "166883258200621056" }
                     ?.let { assertTrue(it.getAnime().contains(Anime("JoJo"))) }
             }
@@ -82,15 +85,22 @@ class UserTests {
 
         // Delete existing user
         withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Delete, "/user/166883258200621056").apply {
+            handleRequest(HttpMethod.Delete, "/user/$token/166883258200621056").apply {
                 assertNull(userStorage.find { it.id == "166883258200621056" })
                 assertEquals(HttpStatusCode.OK, response.status())
             }
         }
 
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Delete, "/user/123456/137453944237457408").apply {
+                assertNotNull(userStorage.find { it.id == "137453944237457408" })
+                assertEquals(HttpStatusCode.Unauthorized, response.status())
+            }
+        }
+
         // Delete non existing user
         withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Delete, "/user/634011684843028509").apply {
+            handleRequest(HttpMethod.Delete, "/$token/user/634011684843028509").apply {
                 assertEquals(HttpStatusCode.NotFound, response.status())
             }
         }
