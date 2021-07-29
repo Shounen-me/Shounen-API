42530e1e5a9ec5f12e124dc1dfecdf40b8d07807
Manuel Kour <manuelkour@gmail.com>
2021-07-29 12:38:35 +0200

    Bot Authorization

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -6,10 +6,8 @@ import io.ktor.serialization.*
 import routes.registerUserRoutes
 
 // TODO Database Integration with PostgreSQL
-// TODO More tests -> Testing every single functionality
 // TODO Profile Picture SFW Authorization
 // TODO MAL or similiar API to search for anime and ratings instead of just accepting anime name
-// TODO Only authorize API calls for authorized Bots via environment variable/text file
 // TODO Check for valid link to picture file for profile picture
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
