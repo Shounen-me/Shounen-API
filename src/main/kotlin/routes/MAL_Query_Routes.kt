a2d72b8cf8a43d8857fb7cb4eb9383c028e386b2
Manuel Kour <manuelkour@gmail.com>
2021-08-19 11:19:19 +0200

    Updated MAL functions & Query Routes

--- src/main/kotlin/routes/MAL_Query_Routes.kt
+++ src/main/kotlin/routes/MAL_Query_Routes.kt
@@ -7,40 +7,38 @@ import io.ktor.routing.*
 import src.main.kotlin.sync.MAL_Client
 import src.main.kotlin.utils.SecretUtils.authorized_token
 
+// http://localhost:8080/mal/anime/mfeWxfHNFQ/166883258200621056/44074/11
 fun Route.postAnime() {
-    post("/mal/{token}/{discordID}/anime/{id}/{episodes}}") {
-        if (authorized_token.contains(call.parameters["token"])) {
+    post("/mal/anime/{token}/{discordID}/{id}/{episodes}") {
+        if (!authorized_token.contains(call.parameters["token"])) {
             call.respondText("Unauthorized access.", status = HttpStatusCode.Unauthorized)
         } else {
             val animeID = call.parameters["id"]!!
             val discordID = call.parameters["discordID"]!!
-            if (call.parameters["episodes"] != null)
-                call.parameters["episodes"]?.let { it1 -> MAL_Client(discordID).postAnime(animeID.toLong(), it1.toInt()) }
-            else
-                MAL_Client(discordID).postAnime(animeID.toLong())
+            val episodeCount = call.parameters["episodes"]!!.toInt()
+            MAL_Client(discordID).postAnime(animeID.toLong(), episodeCount)
             call.respondText("Anime added successfully.", status = HttpStatusCode.OK)
         }
     }
 }
 
 fun Route.postManga() {
-    post("/mal/{token}/{discordID}/manga/{id}}") {
+    post("/mal/manga/{token}/{discordID}/{id}/{volumes}/{chapters}") {
         if (authorized_token.contains(call.parameters["token"])) {
             call.respondText("Unauthorized access.", status = HttpStatusCode.Unauthorized)
         } else {
             val mangaID = call.parameters["id"]!!
             val discordID = call.parameters["discordID"]!!
-            if (call.parameters["episodes"] != null)
-                call.parameters["episodes"]?.let { it1 -> MAL_Client(discordID).postAnime(mangaID.toLong(), it1.toInt()) }
-            else
-                MAL_Client(discordID).postAnime(mangaID.toLong())
+            val chapterCount = call.parameters["chapters"]!!.toInt()
+            val volumeCount = call.parameters["volumes"]!!.toInt()
+            MAL_Client(discordID).postManga(mangaID.toLong(), volumeCount, chapterCount)
             call.respondText("Manga added successfully.", status = HttpStatusCode.OK)
         }
     }
 }
 
 fun Route.getAnimeAndManga() {
-    get("/mal/{discordID}/{type}/{id}") {
+    get("/mal/info/{discordID}/{type}/{id}") {
         val type = call.parameters["type"]!!
         val id = call.parameters["id"]!!.toLong()
         val discordID = call.parameters["discordID"]!!
--- src/main/kotlin/sync/MAL_Client.kt
+++ src/main/kotlin/sync/MAL_Client.kt
@@ -1,9 +1,16 @@
 package src.main.kotlin.sync
 
+import com.kttdevelopment.mal4j.AccessToken
 import com.kttdevelopment.mal4j.MyAnimeList
 import com.kttdevelopment.mal4j.MyAnimeListAuthenticator
 import com.kttdevelopment.mal4j.anime.Anime
+import com.kttdevelopment.mal4j.anime.AnimeListStatus
+import com.kttdevelopment.mal4j.anime.property.AnimeStatus
+import com.kttdevelopment.mal4j.anime.property.RewatchValue
 import com.kttdevelopment.mal4j.manga.Manga
+import com.kttdevelopment.mal4j.manga.property.MangaStatus
+import com.kttdevelopment.mal4j.manga.property.RereadValue
+import com.kttdevelopment.mal4j.property.Priority
 import src.main.kotlin.database.DatabaseAccess
 import src.main.kotlin.utils.SecretUtils.clientId
 import src.main.kotlin.utils.SecretUtils.clientSecret
@@ -11,19 +18,40 @@ import src.main.kotlin.utils.SecretUtils.clientSecret
 class MAL_Client(discordID: String) {
 
     private val access = DatabaseAccess().getAccess(discordID)
-    private val mal: MyAnimeList =
-        MyAnimeList.withAuthorization(MyAnimeListAuthenticator(clientId, clientSecret, access.code, access.verifier))
+
+    private val mal: MyAnimeList = MyAnimeList.withAuthorization(MyAnimeListAuthenticator(
+        clientId, clientSecret, access.code, access.verifier))
 
     fun getUserName() : String = mal.authenticatedUser.name
 
-    fun postAnime(animeID: Long) = mal.updateAnimeListing(animeID).update()
-    fun postAnime(animeID: Long, episodes: Int) = mal.updateAnimeListing(animeID).episodesWatched(episodes).update()
+    fun postAnime(animeID: Long, episodes: Int, rewatching: Boolean = false,
+                  timesRewatched: Int = 0, rewatchValue: RewatchValue = RewatchValue.High) {
+        mal.updateAnimeListing(animeID)
+            .status(AnimeStatus.Completed)
+            .episodesWatched(episodes)
+            .rewatching(rewatching)
+            .priority(Priority.High)
+            .timesRewatched(timesRewatched)
+            .rewatchValue(rewatchValue)
+            .update()
+    }
 
-    fun postManga(mangaID: Long) = mal.updateMangaListing(mangaID).update()
-    fun postManga(mangaID: Long, chapters: Int) = mal.updateMangaListing(mangaID).chaptersRead(chapters).update()
+    fun postManga(mangaID: Long, volumes: Int, chapters: Int) {
+        mal.updateMangaListing(mangaID)
+            .status(MangaStatus.Completed)
+            .volumesRead(volumes)
+            .chaptersRead(chapters)
+            .rereading(false)
+            .priority(Priority.High)
+            .rereadValue(RereadValue.High)
+            .update()
+    }
 
-    fun getAnime(id: Long) : Anime = mal.getAnime(id)
+    fun getAnime(id: Long): Anime = mal.getAnime(id)
 
     fun getManga(id: Long) : Manga = mal.getManga(id)
 
+    fun refresh(): AccessToken = MyAnimeListAuthenticator(
+        clientId, clientSecret, access.code, access.verifier
+    ).refreshAccessToken()
 }
